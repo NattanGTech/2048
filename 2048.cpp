70d7053ea190c6b91ace1dacfd0f8c6062e0acf0
Fearful07 <117281645+Fearful07@users.noreply.github.com>
2023-10-24 17:25:07 +0200

    Update 2048.cpp

--- 2048.cpp
+++ 2048.cpp
@@ -1,13 +1,27 @@
 #include <iostream>
-
+#include <string>
+#include <vector>
 #include "case.h"
 #include "grid.h"
-
+#include <stdlib.h> 
+#include <time.h>
+#include <windows.h>
 
 int main()
 {
+    int flag = 1;
 
+    while (flag) {
+        if (GetKeyState(VK_LBUTTON) & 0x8000) {
+            std::cout << "Right Shift key pressed" << std::endl;
+            flag = 0;
+        }
+    }
+	srand (time(NULL));
 	Grid* oGrid = new Grid();
-	std::cout << oGrid;
+	oGrid->display();
 
+
+	delete oGrid;
+	return 0;
 }
