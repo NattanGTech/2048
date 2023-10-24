472a41139f91ec80b77e2ac49ff634c43ab6b034
Fearful07 <117281645+Fearful07@users.noreply.github.com>
2023-10-24 21:27:28 +0200

    Update 2048.cpp

--- 2048.cpp
+++ 2048.cpp
@@ -7,21 +7,45 @@
 #include <time.h>
 #include <windows.h>
 
+#define KEY_UP 72
+#define KEY_DOWN 80
+#define KEY_LEFT 75
+#define KEY_RIGHT 77
+
 int main()
 {
+    srand(time(NULL));
+    Grid* oGrid = new Grid();
+    oGrid->display();
+    bool bGameActive = true;
     int flag = 1;
 
     while (flag) {
         if (GetKeyState(VK_LBUTTON) & 0x8000) {
-            std::cout << "Right Shift key pressed" << std::endl;
+            oGrid->leftMovement();
+            oGrid->newTiles();
+            oGrid->display();
+            flag = 0;
+        }
+        else if (GetKeyState(VK_RBUTTON) & 0x8000) {
+            oGrid->rightMovement();
+            oGrid->newTiles();
+            oGrid->display();
+            flag = 0;
+        }
+        else if (GetKeyState(VK_TAB) & 0x8000) {
+            oGrid->upMovement();
+            oGrid->newTiles();
+            oGrid->display();
+            flag = 0;
+        }
+        else if (GetKeyState(VK_SHIFT) & 0x8000) {
+            oGrid->downMovement();
+            oGrid->newTiles();
+            oGrid->display();
             flag = 0;
         }
     }
-	srand (time(NULL));
-	Grid* oGrid = new Grid();
-	oGrid->display();
-
-
 	delete oGrid;
 	return 0;
 }
