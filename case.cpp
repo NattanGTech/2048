2e027400ecc8cf5effd20c581a4e043578cfa004
Fearful07 <117281645+Fearful07@users.noreply.github.com>
2023-10-24 17:24:48 +0200

    Update case.cpp

--- case.cpp
+++ case.cpp
@@ -1,19 +1,9 @@
-#include <iostream>
-#include <stdlib.h>
 #include "case.h";
-#include "grid.cpp";
+#include <iostream>
 
 Case::Case()
 {
-	int m_iValue;
-	int m_iPosition;
-	char m_iSymbol;
-	init();
+	m_iValue = 0;
+	m_cSymbol = " ";
 }
 
-void init() {
-	int iPossibleValue[2] = [2, 4];
-	m_iValue = iPossibleValue[rand() % 2];
-	m_iPosition = m_vifreePosition[rand() % m_vifreePosition.length()];
-	m_iSymbol = to_string(m_iValue).c_str();
-}
