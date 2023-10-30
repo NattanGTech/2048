0a070afb5dbed87441322d0f4dcb6e1a640d5551
NattanGTech <117281720+NattanGTech@users.noreply.github.com>
2023-10-30 12:00:22 +0100

    Update gameObject.h

--- SDL/gameObject.h
+++ SDL/gameObject.h
@@ -1,9 +1,20 @@
 #pragma once
+#include <SDL.h>
+//class SDL_Renderer;
+//struct SDL_Color;
 
-class GameObject {
-	GameObject(int iX, int iY, int iSizeX, int iSizeY);
-
+class GameObject 
+{
 public:
+	int m_iX;
+	int m_iY;
+	int m_iWidth;
+	int m_iLength;
+	SDL_Color m_sdlColor;
+
+	GameObject(int iX, int iY, int iWidth, int iLength, SDL_Color sdlColor);
+
+	void drawRect(SDL_Renderer* sdlRenderer);
+
 	~GameObject();
-};
 };
