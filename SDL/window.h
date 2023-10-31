71842c6fd7342b091ffbdfa95e95361813ca32b7
Fearful07 <117281645+Fearful07@users.noreply.github.com>
2023-10-31 13:05:13 +0100

    Update window.h

--- SDL/window.h
+++ SDL/window.h
@@ -1,17 +1,25 @@
 #pragma once
+
+#include <vector>
+
 struct SDL_Window;
 struct SDL_Renderer;
+
+class GameObject;
 
 class Window
 {
 public:
 	SDL_Window* m_sdlWindow;
 	SDL_Renderer* m_sdlRenderer;
+	std::vector<GameObject*> m_voGameObjects;
+
 
 	Window();
 
-	void close();
 	void display();
+	void close();
 
 	~Window();
 };
+
