4495573796cf6740dc3b15e465e9d9988c8f4c7a
NattanGTech <117281720+NattanGTech@users.noreply.github.com>
2023-11-01 16:28:45 +0100

    Update case.h

--- SDL/case.h
+++ SDL/case.h
@@ -1,17 +1,25 @@
 #pragma once
 #include "gameObject.h";
 
+#include <SDL.h>
+#include <map>
 #include <string>
 
+struct SDL_Color;
 
 class Case : GameObject
 {
+private:
+
+	std::map<int, SDL_Color> m_mColors;
+
 public:
 
 	int m_iValue;
 
-	Case(int iX, int iY, int iWitdh, int iLength, SDL_Color sdlColor, Window* oWindow);
+	Case(std::map<int,SDL_Color> mColors, int iX, int iY, int iWitdh, int iLength, SDL_Color sdlColor, Window* oWindow);
 
 	void display(int iMax);
+	void setColor();
 
 };
